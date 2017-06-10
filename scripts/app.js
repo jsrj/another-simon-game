83d5d631c3a300a50a6560d43cd7e8de08004a3f
jsrj <rj.javadev@icloud.com>
2017-06-10 10:43:44 -0400

    Setting up the Simon game clone.

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// require('bootstrap');
+
+console.log("Does \'snek\' count as a string?");
+$(document).ready(function() {
+
+
+/* All the Coffee and Sugar */
+
+
+/* No more Coffee and Sugar */
+
+
+});
---
+++ scripts/simon.js
@@ -0,0 +1,13 @@
+// require('bootstrap');
+
+console.log("Does \'simon says\' count as a string?");
+$(document).ready(function() {
+
+
+/* All the Coffee and Sugar */
+
+
+/* No more Coffee and Sugar */
+
+
+});
