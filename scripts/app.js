cf9e5297cd710d9dc41b44a5ed91d78f56d505c9
jsrj <rj.javadev@icloud.com>
2017-06-10 12:47:19 -0400

    Initializing of javascripts. There is a bug wherein the game object reports undefined if defined in app.js instead of simon.js, so this will need to be fixed.

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-
   console.log("Does \'snek\' count as a string?");
+$(document).ready(function() {
+  console.log('dom ready');
+
 
 
 /* All the Coffee and Sugar */
 
-    var myGame = new SimonGame();
+    // var myGame2 = new SimonGame();
+    // myGame.startGame();
+    //
+    // console.log(myGame2);
 
 /* No more Coffee and Sugar */
 
--- scripts/simon.js
+++ scripts/simon.js
@@ -1,15 +1,41 @@
 $(document).ready(function() {
 
-  console.log("Does \'simon says\' count as a string?");
-
 
 /* All the Coffee and Sugar */
 
+
     function SimonGame () {
+      this.colors = ['red', 'green', 'blue', 'yellow'];
+      // Our current sequence
+      this.sequence = [];
+      // Where the user is in the sequence.
+      this.userClickCount = 0;
+      // What the player's score is
+      this.round = 1;
+    }
+    // Starts the Simon game
+    SimonGame.prototype.startGame = function(){
+      console.log('Starting the game...');
+      this.addColor();
+      this.showSequence();
+    };
+
+    // Chooses one of the 4 colors at random and adds it to the sequence
+    SimonGame.prototype.addColor = function () {
+      var randomIndex = Math.floor(Math.random() * 4);
+      this.sequence.push(this.colors[randomIndex]);
+    };
+
+    // Blinks the buttons based on the current color sequence
+    SimonGame.prototype.showSequence = function () {
 
     }
 
+
+    var myGame = new SimonGame();
+    myGame.startGame();
+    console.log(myGame);
+
+
 /* No more Coffee and Sugar */
-
-
 });
